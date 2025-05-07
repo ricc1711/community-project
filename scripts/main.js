b5c2f6e9caeeeb2b899706e55c4a22a1b2247c11
Christian Noss <christian.noss@th-koeln.de>
2025-05-07 18:38:37 +0200

    Added new Features and Card Designs

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
 const templateEreigniskarte = `
-  <li class="ereigniskarte card">
+  <li class="ereigniskarte card is-scaleable">
 
     <figure class="flex flex-col h-full">
-      <img src="{{image}}" alt="Bild">
+
 
       <figcaption class="card-body flex flex-col">
-        <h3 class="card-title pb-4 flex-grow">{{title}}</h3>
+        <h3 class="card-title pb-4">{{title}}</h3>
           <a href="{{url}}" target="_blank"></a>
         <div class="ereigniskarte__desc">
-          {{desc}}
+          {{{desc}}}
         </div>
         <dl class="border-t border-t-2 pt-4 border-pink-600">
           {{#points}}
@@ -27,16 +27,49 @@ const templateAktionen = `
     <figure class="flex flex-col h-full">
     
       <figcaption class="card-body flex flex-col">
-        <h3 class="card-title pb-4 flex-grow">{{title}}</h3>
-          <a href="{{url}}" target="_blank"></a>
-        <div class="">
-        </div>
+        <h3 class="card-title pb-4"><a href="{{url}}" target="_blank" class="btn btn-primary mt-4">{{title}}</a></h3>
+        
+        <div class="flex-grow pb-5">{{{body}}}</div>
+
         <dl class="border-t border-t-2 pt-4 border-pink-600">
           {{#points}}
-          <dt class="inline">Punkte</dt>
+          <dt class="inline"><span class="text-xl">⏱️</span> Punkte:</dt>
           <dd class="inline">{{points}}</dd>
           {{/points}}
+
+          {{#teamSize}}
+          <dt class="inline"><span class="text-xl">👫</span> Teamgröße:</dt>
+          <dd class="inline">{{teamSize}}</dd>
+          {{/teamSize}}
         </dl>
+
+      </figcaption>
+    </figure>
+  </li>
+`;
+
+const templateAktionenBacklog = `
+  <li class="aktionskarte card">
+
+    <figure class="flex flex-col h-full">
+    
+      <figcaption class="card-body flex flex-col">
+        <h3 class="card-title pb-4"><a href="{{url}}" target="_blank" class="btn btn-primary mt-4">{{title}}</a></h3>
+        
+        <div class="flex-grow pb-5">{{{body}}}</div>
+
+        <dl class="border-t border-t-2 pt-4 border-pink-600">
+          {{#points}}
+          <dt class="inline"><span class="text-xl">⏱️</span> Punkte:</dt>
+          <dd class="inline">{{points}}</dd>
+          {{/points}}
+
+          {{#teamSize}}
+          <dt class="inline"><span class="text-xl">👫</span> Teamgröße:</dt>
+          <dd class="inline">{{teamSize}}</dd>
+          {{/teamSize}}
+        </dl>
+
       </figcaption>
     </figure>
   </li>
@@ -44,7 +77,8 @@ const templateAktionen = `
 
 const templates = {
   ereigniskarten: templateEreigniskarte,
-  aktionen: templateAktionen
+  aktionen: templateAktionen,
+  aktionenBacklog: templateAktionenBacklog
 }
 
 async function fetchItemsFromApi() {
@@ -64,7 +98,7 @@ function truncate(str, max = 250) {
 
 function getBodyText(body) {
   if (!body) return '';
-  
+
   // bild entfernen
   const bodyWithoutImage = body.replace(/!\[.*?\]\(.*?\)/g, '');
   return bodyWithoutImage;
@@ -83,25 +117,34 @@ function getImageFromBody(body) {
   }
 }
 
-  
+const prepareBodyText = (bodyText) => {
+  if (!bodyText) return '';
+
+  const textShort = truncate(bodyText, 250);
+  const textWithProperHeadlines = textShort.replace(/## /gm, '#### ' );
+  return textWithProperHeadlines;
+};
+
 
 function renderItems(items, target) {
 
   const targetElement = document.getElementById(target);
   const template = templates[target];
-  console.log(target)
+
   items.forEach(item => {
+    if (!item.content) return;
     
     const content = item.content;
     if (!content) return;
 
+    const md = markdownit()
     const title = content.title;
     const url = content.url;
-    const body = content.body;
+    const bodyText = prepareBodyText(content.body);
+    const body = md.render(bodyText);
     const labels = content.labels.nodes;
     const desc = getBodyText(body);
     const image = getImageFromBody(body);
-
 
     const tags = labels.map(label => {
       return {
@@ -109,28 +152,36 @@ function renderItems(items, target) {
         color: label.color
       };
     });
-
+    
     const points = labels.filter(label => {
-      
       return label.name.match(/Points/);
-    }
-    ).map(label => {
+    }).map(label => {
       const match = label.name.match(/\: (.*)/); // erste Zahl suchen
-      const number = match ? match[0] : null;
+      const number = match ? match[1] : 0;
       return number;
     });
 
+    const teamSizeArray = labels.filter(label => {
+      return label.name.match(/Teamsize/);
+    }).map(label => {
+      const match = label.name.match(/\: (.*)/); // erste Zahl suchen
+      const number = match ? match[1] : 0;
+      return number;
+    });
+
+    const teamSize = teamSizeArray.join(', ');
+    
     const itemData = {
       title,
       url,
       desc,
       tags,
       points,
-      image
+      teamSize,
+      image,
+      body
     };
 
-    console.log(itemData)
-    
     const renderedItem = Mustache.render(template, itemData);
     targetElement.innerHTML += renderedItem;
   });
@@ -139,11 +190,12 @@ function renderItems(items, target) {
 
 function processItems(itemStack) {
   renderItems(itemStack.ereigniskarten, 'ereigniskarten');
-  renderItems(itemStack.aktionskarten, 'aktionen');
-
+  renderItems(itemStack.verifizierteAktionen, 'aktionen');
+  renderItems(itemStack.aktionenBacklog, 'aktionenBacklog');
 }
 
 function assignItems(items) {
+
   const ereigniskarten = items.filter(item => {
     return item.content.labels.nodes.some(node => {
       return node.name === 'Ereigniskarte';
@@ -151,12 +203,27 @@ function assignItems(items) {
   });
 
   const aktionskarten = items.filter(item => {
+
     return !item.content.labels.nodes.some(node => {
       return node.name === 'Ereigniskarte';
     });
   });
 
-  return {ereigniskarten, aktionskarten};
+  const verifizierteAktionen = aktionskarten.filter(item => {
+    return item.fieldValues.nodes.some(node => {
+      return node.name && node.name === "Verified";
+    });
+  });
+
+  const aktionenBacklog = aktionskarten.filter(item => {
+    return item.fieldValues.nodes.some(node => {
+      return node.name && node.name === "ToBeVerified";
+    });
+  });
+
+
+
+  return { ereigniskarten, verifizierteAktionen, aktionenBacklog };
 }
 
 fetchItemsFromApi()
